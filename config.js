5e142959d67ed1959c9441cf9f97f8dfeb5e43f0
7K Sanz <123192044+7K-Sanz@users.noreply.github.com>
2023-03-09 00:38:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // S C R I P T  O R I  B Y  Bochilgaming And KANNACHAN 🔭
-// Ditulis Ulang Oleh WH MODS DEV
+// Ditulis Ulang Oleh Sanzz Dev
 // Credits, jangan dihapus atau diubah!
 
 // - - THANKS TO - -
@@ -13,6 +13,7 @@
 // • Rlxfly
 // • Rasel comel
 // • ImYanXiao
+// • Sanzz Dev
 // • WH MODS DEV
 // • Xtreshe
 // • Dll
@@ -49,22 +50,22 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
-global.sig = 'https://instagram.com/inizenscuy' // ig
-global.sgh = 'https://github.com/RaaaGH/' // github
-global.sgc = 'https://chat.whatsapp.com/' // group whatsapp
-global.sdc = 'https://www.discord.com'//discord
+global.sig = '-' // ig
+global.sgh = 'https://github.com/7K-Sanzz' // github
+global.sgc = 'https://chat.whatsapp.com/HUTpnP78RmJI6zpGXi9Off' // group whatsapp
+global.sdc = '-'//discord
 global.snh = 'https://nhentai.net/g/767842' //Hmmm, Ini boleh kalian isi terserah :v
-global.sfb = 'https://www.facebook.com/' // facebook
-global.syt = 'https://youtube.com/channel/UCTVeRHM0v0Hqv_-O8yVkFpw' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
+global.sfb = '-' // facebook
+global.syt = 'https://youtube.com/@7KSanzz' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
 
 /*============== PAYMENT ==============*/
-global.pdana = '62895604670507' //dana
-global.povo = '62895604670507' //ovo
-global.pgopay = '62895604670507' //gopay
-global.plinkaja = '62895604670507' //link aja
-global.ppulsa = '62895604670507' //telkomsel
-global.ppulsa2 = '62895604670507' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.com/'
+global.pdana = '0895600740052' //dana
+global.povo = '0895600740052' //ovo
+global.pgopay = '0895600740052' //gopay
+global.plinkaja = '-' //link aja
+global.ppulsa = '0895600740052' //tri
+global.ppulsa2 = '087748210565' // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.com/sanzz'
 
 /*============== NOMOR ==============*/
 global.nomorbot = '62XXXXXXXXXXX'
@@ -73,8 +74,8 @@ global.namebot = 'AlbedoBotz'
 global.nameown = 'Zenss'
 
 /*============== Github Thanks To ==============*/
-global.namagithub = 'RaaaGH' // nama github lu
-global.linkgithub = 'github.com/RaaaGH' // tarok link github lu
+global.namagithub = '7K Sanzz' // nama github lu
+global.linkgithub = 'github.com/7K-Sanzz' // tarok link github lu
 
 // klau kgk ada github kasih tanda '-' aja biar kgk error
 
@@ -85,8 +86,8 @@ global.lolkey = 'e54205a4ca2caa368cc067bb' // PUNYA WAHID  8e66d0934cf741bfd2182
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['62895604670507', 'Zenss', true], 
-  ['-', 'ISI KALAU ADA', true],
+  ['62877482105655', '7K Sanzz', true], 
+  ['62895600740052', 'VansZann', true],
   ['-', 'ISI KALAU ADA', true],
   ['-', 'ISI KALAU ADA', true]
     //Ganti jd nomormu sama Namamu
@@ -137,14 +138,14 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 Albedo-BOT 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Albedo-BOT ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm = '                「 Vanz-BOT 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Vanz-BOT ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
 global.wm3 = '⫹⫺ Albedo-BOT '
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | By Zenss`
-global.packname = 'Albedo-Botz WhatsApp'
-global.author = '                「 Albedo-BOT  あ」'
+global.titlebot = `⫹⫺ WhatsApp Bot | By 7K Sanzz`
+global.packname = 'Vanz-Botz WhatsApp'
+global.author = '                「  VANZ BOT  あ」'
 
 /*Fake*/
 global.fsizedoc = '1'.repeat(10)
@@ -635,8 +636,8 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = 'Albedo-BOT'
-global.stickauth = `By ©WH-MODS-DEV`
+global.stickpack = 'Vanz-BOT'
+global.stickauth = `By ©Sanzz-DEV`
 
 global.multiplier = 38 // The higher, The harder levelup
 
